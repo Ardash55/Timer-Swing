6a0e5d5393f704007b3e04942edd886a368fb204
Ardash55 <ardash30042005g@gmail.com>
2025-06-05 15:57:34 +0300

    Изменил вывод актуального времени. Добавил кнопку сбросаа и соответствующий метод

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime timeNow = LocalTime.now();
+//        LocalTime timeNow = LocalTime.now();
 //        LocalTime timeNow = now.format(timeFormatter);
 
         JFrame frame = new JFrame();
@@ -18,7 +18,7 @@ public class Main {
         panel.setLayout(null);
         panel.setSize(800, 600);
 
-        JLabel timeLabel = new JLabel("ththth");
+        JLabel timeLabel = new JLabel("Выберите функцию");
         panel.add(timeLabel);
         timeLabel.setBounds(0, 0, 800, 100);
 
@@ -27,7 +27,16 @@ public class Main {
         timeNowButton.setBounds(100, 100, 150, 70);
         timeNowButton.setFont(new Font("Arial", Font.PLAIN, 15));
         timeNowButton.addActionListener(e -> {
+            LocalTime timeNow = LocalTime.now();
             timeLabel.setText("Время " + timeNow.format(timeFormatter));
+        });
+
+        JButton resetButton = new JButton("Сбросить");
+        panel.add(resetButton);
+        resetButton.setBounds(100, 200, 150, 70);
+        resetButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        resetButton.addActionListener(e -> {
+            timeLabel.setText("Выберите функцию");
         });
 
         frame.setVisible(true);
